3fa4db35f434a5d7b2ee3f95f1625437cb0e2051
Yanfth <alfianfatoni1405@gmail.com>
2026-05-11 15:29:27 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const inputTugas = document.getElementById("inputTugas");
+const btnTambah = document.getElementById("btnTambah");
+const daftarTugas = document.getElementById("daftarTugas");
+
+btnTambah.addEventListener("click", function () {
+  let teksTugas = inputTugas.value;
+
+  if (teksTugas === "") {
+    alert("Data harus dimasukkan!");
+    return;
+  }
+
+  let listbaru = document.createElement("li");
+  let spanbaru = document.createElement("span");
+
+  spanbaru.innerHTML = teksTugas;
+
+  listbaru.appendChild(spanbaru);
+
+  daftarTugas.appendChild(listbaru);
+
+  const warnabaru = document.querySelectorAll("li");
+  warnabaru.forEach((item, index) => {
+    if (index % 2 === 0) {
+      item.style.color = "red";
+    } else {
+      item.style.color = "green";
+    }
+  });
+
+  inputTugas.value = "";
+});
