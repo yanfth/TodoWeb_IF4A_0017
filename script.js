34490f8cdeed5343e10dec8171b9ec70a4947d87
Yanfth <alfianfatoni1405@gmail.com>
2026-05-11 17:46:16 +0700

    Meembuat script tambahan untuk axis

--- script.js
+++ script.js
@@ -106,3 +106,24 @@ btnTambah.addEventListener("click", function () {
   inputTugas.value = "";
   inputTanggal.value = "";
 });
+
+const card = document.querySelector(".card");
+
+card.addEventListener("mousemove", function (e) {
+  const rect = card.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  const centerX = rect.width / 2;
+  const centerY = rect.height / 2;
+
+  const rotateX = ((y - centerY) / centerY) * -8;
+  const rotateY = ((x - centerX) / centerX) * 8;
+
+  card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.01)`;
+});
+
+card.addEventListener("mouseleave", function () {
+  card.style.transform =
+    "perspective(1000px) rotateX(0deg) rotateY(0deg) scale(1)";
+});
