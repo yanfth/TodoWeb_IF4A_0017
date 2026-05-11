c6532b9a8a779822bd7369b0d12bc494e92620b4
Yanfth <alfianfatoni1405@gmail.com>
2026-05-11 16:51:35 +0700

    Menambahkan Tombol Edit tiap List

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const inputTanggal = document.getElementById("inputTanggal");
 const btnTambah = document.getElementById("btnTambah");
 const daftarTugas = document.getElementById("daftarTugas");
 
+let editItem = null;
+
 btnTambah.addEventListener("click", function () {
   let teksTugas = inputTugas.value;
   let tanggal = inputTanggal.value;
@@ -15,11 +17,18 @@ btnTambah.addEventListener("click", function () {
   let listbaru = document.createElement("li");
   let spanbaru = document.createElement("span");
   spanbaru.innerHTML = teksTugas;
+  spanbaru.classList.add("teks-tugas");
 
   let spanTanggal = document.createElement("span");
   spanTanggal.innerHTML = ` ${tanggal}`;
   spanTanggal.classList.add("tanggal");
 
+  // Tombol Edit
+  let btnEdit = document.createElement("button");
+  btnEdit.innerHTML = "Edit";
+  btnEdit.classList.add("edit");
+
+  // Tombol Hapus
   let btnHapus = document.createElement("button");
   btnHapus.innerHTML = "Hapus";
   btnHapus.classList.add("hapus");
@@ -28,9 +37,14 @@ btnTambah.addEventListener("click", function () {
     daftarTugas.removeChild(listbaru);
   });
 
+  let divTombol = document.createElement("div");
+  divTombol.classList.add("tombol-group");
+  divTombol.appendChild(btnEdit);
+  divTombol.appendChild(btnHapus);
+
   listbaru.appendChild(spanbaru);
   listbaru.appendChild(spanTanggal);
-  listbaru.appendChild(btnHapus);
+  listbaru.appendChild(divTombol);
   daftarTugas.appendChild(listbaru);
 
   inputTugas.value = "";
