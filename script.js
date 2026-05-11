8e2cee8ce874e79fa7bb6fb59922aecef02e26cb
Yanfth <alfianfatoni1405@gmail.com>
2026-05-11 16:57:57 +0700

    Memberikan fungsi edit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const daftarTugas = document.getElementById("daftarTugas");
 let editItem = null;
 
 btnTambah.addEventListener("click", function () {
-  let teksTugas = inputTugas.value;
+  let teksTugas = inputTugas.value.trim();
   let tanggal = inputTanggal.value;
 
   if (teksTugas === "" || tanggal === "") {
@@ -14,21 +14,41 @@ btnTambah.addEventListener("click", function () {
     return;
   }
 
+  if (editItem !== null) {
+    editItem.querySelector(".teks-tugas").innerHTML = teksTugas;
+    let spanTgl = editItem.querySelector(".tanggal");
+    spanTgl.innerHTML = ` ${tanggal}`;
+    spanTgl.dataset.nilai = tanggal;
+
+    editItem = null;
+    btnTambah.innerHTML = "Tambah Data";
+    inputTugas.value = "";
+    inputTanggal.value = "";
+    return;
+  }
+
   let listbaru = document.createElement("li");
+
   let spanbaru = document.createElement("span");
   spanbaru.innerHTML = teksTugas;
   spanbaru.classList.add("teks-tugas");
 
   let spanTanggal = document.createElement("span");
   spanTanggal.innerHTML = ` ${tanggal}`;
+  spanTanggal.dataset.nilai = tanggal;
   spanTanggal.classList.add("tanggal");
 
-  // Tombol Edit
   let btnEdit = document.createElement("button");
   btnEdit.innerHTML = "Edit";
   btnEdit.classList.add("edit");
 
-  // Tombol Hapus
+  btnEdit.addEventListener("click", function () {
+    inputTugas.value = spanbaru.innerHTML;
+    inputTanggal.value = spanTanggal.dataset.nilai;
+    editItem = listbaru;
+    btnTambah.innerHTML = "Simpan Edit";
+  });
+
   let btnHapus = document.createElement("button");
   btnHapus.innerHTML = "Hapus";
   btnHapus.classList.add("hapus");
